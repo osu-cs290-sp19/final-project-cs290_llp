42b5376e434246dbc525b024338c19c1088b26ed
Alexander Smith <smitale2@flip3.engr.oregonstate.edu>
2019-06-10 18:38:44 -0700

    edit5

--- public/postTemplate.js
+++ public/postTemplate.js
@@ -11,10 +11,10 @@ templates['post'] = template({"compiler":[7,">= 4.0.0"],"main":function(containe
     + alias4(((helper = (helper = helpers.reply || (depth0 != null ? depth0.reply : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"reply","hash":{},"data":data}) : helper)))
     + " </a>\n	</p>\n    <p class=\"post-text\">\n      "
     + alias4(((helper = (helper = helpers.text || (depth0 != null ? depth0.text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"text","hash":{},"data":data}) : helper)))
-    + "\n    </p>\n    <p class=\"post-author\">\n      <a href=\"#\">"
+    + "\n    </p>\n    <p class=\"post-author\">\n      "
     + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data}) : helper)))
-    + "</a>\n    </p>\n	<button class=\"button\">"
+    + "\n    </p>\n	<p class=\"post-ID\">\n	  "
     + alias4(((helper = (helper = helpers.ID || (depth0 != null ? depth0.ID : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ID","hash":{},"data":data}) : helper)))
-    + "</button>\n  </div>\n</article>";
+    + "\n	</p>\n  </div>\n</article>";
 },"useData":true});
 })();
